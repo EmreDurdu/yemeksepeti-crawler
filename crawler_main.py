6d51650bdea8ea955620b9b8c314b47b1bce5abe
Muhammed Emre Durdu <muhammedemredurdu@gmail.com>
2020-12-01 10:52:07 +0300

    hotfix

--- crawler_main.py
+++ crawler_main.py
@@ -4,16 +4,16 @@ from twisted.internet import reactor, defer
 from CommentMatcher import CommentMatcher
 from Spiders import CommentSpider, CitiesSpider, DistrictSpider, RestaurantSpider, MenuSpider
 
-runner = CrawlerRunner()
+process = CrawlerProcess({'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'})
 
 
 @defer.inlineCallbacks
 def crawl():
-    yield runner.crawl(CitiesSpider)
-    yield runner.crawl(DistrictSpider)
-    yield runner.crawl(RestaurantSpider)
-    yield runner.crawl(MenuSpider)
-    yield runner.crawl(CommentSpider)
+    yield process.crawl(CitiesSpider)
+    yield process.crawl(DistrictSpider)
+    yield process.crawl(RestaurantSpider)
+    yield process.crawl(MenuSpider)
+    yield process.crawl(CommentSpider)
     reactor.stop()
 
 
@@ -21,7 +21,6 @@ if __name__ == '__main__':
     print(
         "Note that, since the scrapping takes too much time, this is implemented only for Ankara."
         " If you want other cities too, you need to edit Spiders.py file")
-    # process = CrawlerProcess({'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'})
     # process.crawl(CitiesSpider)
     # process.crawl(DistrictSpider)
     # process.crawl(RestaurantSpider)
